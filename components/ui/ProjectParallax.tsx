5c074e85a3a276d8c2d683eed28991ba09d6fa1b
rto24 <richardto180@gmail.com>
2024-10-07 16:43:01 -0400

    set width to take up full width

--- components/ui/ProjectParallax.tsx
+++ components/ui/ProjectParallax.tsx
@@ -57,7 +57,7 @@ export const HeroParallax = ({
   return (
     <div
       ref={ref}
-      className="h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
+      className="h-[300vh] py-40 overflow-x-visible antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]"
     >
       <Header />
       <motion.div
@@ -146,7 +146,7 @@ export const ProductCard = ({
           src={product.thumbnail}
           height="600"
           width="600"
-          className="object-cover object-left-top absolute h-full w-full inset-0"
+          className="object-cover object-center absolute h-full w-full inset-0"
           alt={product.title}
         />
       </Link>
